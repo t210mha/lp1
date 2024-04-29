61f1ac430c5a5f9611902d711d8e73b531b55e07
t210mha <t210mha@gmail.com>
2024-04-30 00:01:27 +0900

    cover、course 作成

--- assets/script/main.js
+++ assets/script/main.js
@@ -5,6 +5,14 @@ $('.header__hamburger-button').on('click', function () {
     // .hamburgerの表示・非表示を繰り返す
     hamburger.toggleClass('header__hamburger-menu--active');
 });
+// リンクをクリックしたら
+$(window).on('click', function (e) {
+    // クリックした場所がリンクであれば
+    if ($(e.target).closest('a').length) {
+        // ハンバーガーメニューを閉じる
+        hamburger.removeClass('header__hamburger-menu--active');
+    }
+});
 // 画面幅のサイズが変わったら
 $(window).on('resize', function () {
     // ハンバーガーメニューを閉じる
