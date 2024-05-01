778e1d545708ce232e4c737a5604425cf5df8a7c
t210mha <t210mha@gmail.com>
2024-05-01 18:44:13 +0900

    クラス名見直し

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,20 +1,20 @@
 // ---------- ハンバーガーメニュー ----------
-var hamburger = $('.header__hamburger-menu');
+var hamburger = $('.header__nav');
 // OPEN/CLOSEボタンをクリックしたら
 $('.header__hamburger-button').on('click', function () {
     // .hamburgerの表示・非表示を繰り返す
-    hamburger.toggleClass('header__hamburger-menu--active');
+    hamburger.toggleClass('header__nav--active');
 });
 // リンクをクリックしたら
 $(window).on('click', function (e) {
     // クリックした場所がリンクであれば
     if ($(e.target).closest('a').length) {
         // ハンバーガーメニューを閉じる
-        hamburger.removeClass('header__hamburger-menu--active');
+        hamburger.removeClass('header__nav--active');
     }
 });
 // 画面幅のサイズが変わったら
 $(window).on('resize', function () {
     // ハンバーガーメニューを閉じる
-    hamburger.removeClass('header__hamburger-menu--active');
+    hamburger.removeClass('header__nav--active');
 });
